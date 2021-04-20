910e7c628d4dff6c46fd737f1b40f1e42b617daa
Endeaveloper <dolp17@gmail.com>
2021-04-20 16:49:10 +0900

    99

--- study/hello.spring/src/main/java/hello/hellospring/HelloSpringApplication.java
+++ study/hello.spring/src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -8,5 +8,14 @@ public class HelloSpringApplication {
     public static void main(String[] args) {
         SpringApplication.run(HelloSpringApplication.class, args);
         System.out.println("@TEST@");
+
+        for (int i=2; i<10; i++){
+
+            for (int j=1; j<10; j++){
+                System.out.println( i + " X " + j + " = " +(i*j));
+
+            }
+            System.out.println("=========================");
+        }
     }
 }
